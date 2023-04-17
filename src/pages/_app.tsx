e0cae359c2b0908ff1865868cd5e18dbf158e630
Simlex <gettosimlex@gmail.com>
2023-04-17 09:44:56 +0100

    Updated app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,41 @@
 import Layout from '@/components/Layout'
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
-import 'aos/dist/aos.css' 
+import 'aos/dist/aos.css'
+
+// import express from "express"
+// // import collection from "../../mongo"
+// const collection = require('../../mongo');
+// import cors from "cors"
+// const app = express()
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: true }))
+// app.use(cors())
+
+// app.post("/", async(req, res) => {
+//   const{email} = req.body
+
+//   const data = {
+//     email:email
+//   }
+
+//   try{
+//     const check = await collection.findOne({email})
+//     if(check) {
+//       res.json('Email exists!');
+//     } else {
+//       res.json("doesn't exist")
+//       await collection.insertMany({data})
+//     }
+//   }
+//   catch(e) {
+//     res.json("failed");
+//   }
+// })
+
+// app.listen(8000, () => {
+//   console.log('Port connected!');
+// })
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Layout>
